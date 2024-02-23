771073df2458f1aa1538bcf5c89b6aab853fcd68
Arnav Parmar <parmar.arnav179@gmail.com>
2024-02-23 09:44:57 +0530

    __name__==__main__

---
+++ Section 9 - Modules and Packages/__name__&__main__/file.py
@@ -0,0 +1,4 @@
+#An often confusing part of Python is a mysterious line of colde: 
+# if __name__ == "__main__"
+
+#sometimes, when you are importing from a module, you would like to now whether a modules function is being used as an import or if you are using the original.py file of that module
---
+++ Section 9 - Modules and Packages/__name__&__main__/one.py
@@ -0,0 +1,22 @@
+#Python one.py
+#__name__ == "__main__"#this is a built in variable
+#it assigns the "main" string to the variable __name__ when you run it directly
+
+
+def func():
+    print("func() in one.py")
+
+def func1():
+    pass
+
+def func2():
+    pass
+
+if __name__ == "__main__":
+    pass
+    func1()
+    func2()
+    #run the script
+#     print("one.py is being run directly !")
+# else:
+#     print("One.py has been imported")
