b24fed885f6c8ae951057b81ca16cff31b2b922f
Arnav Parmar <parmar.arnav179@gmail.com>
2024-02-24 11:35:47 +0530

    exception handing and unit testing

---
+++ Section 10 - Errors and Exception Handling/simple1.py
@@ -0,0 +1,22 @@
+#Pylint testing
+# a = 1
+# b = 2
+# print(a)
+# print(B)
+
+
+'''
+A very simple Script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
